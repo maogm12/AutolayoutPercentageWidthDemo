b2e4b9664066e12e38d2d0ded6ea980f016a5c12
Guangming Mao <maogm12@gmail.com>
2015-07-15 09:58:45 +0800

    add percentage between two views

--- AutolayoutPercentageWidthDemo/ViewController.swift
+++ AutolayoutPercentageWidthDemo/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
 
         let nib = NSBundle.mainBundle().loadNibNamed("percentage", owner: self, options: nil)
         if !nib.isEmpty {
-            let percentageView = nib[0] as UIView
+            let percentageView = nib[0] as! UIView
             let frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height - 64)
             percentageView.frame = frame
             container.addSubview(percentageView)
